bb214253c981ad2bdfe67b32746b34bb6a985701
Stacy Chen <chiufin@gmail.com>
2016-12-17 11:06:57 +0800

    change color rgba -> hsl

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import _ from 'lodash';
+import Color from 'color';
 
 class App extends Component {
   constructor(props) {
@@ -9,19 +9,23 @@ class App extends Component {
     this.state = {
       date: new Date(),
       colors: [
-        {rgba: '#199'},
-        {rgba: '#919'},
-        {rgba: '#991'},
-        {rgba: '#469'}
+        {rgba: 'hsl(255, 70%, 70%)'},
+        {rgba: 'hsl(155, 70%, 70%)'},
+        {rgba: 'hsl(215, 70%, 70%)'},
+        {rgba: 'hsl(50, 70%, 70%)'}
       ]
     };
     this.createColor = this.createColor.bind(this);
     this.deleteColor = this.deleteColor.bind(this);
+    this.lighter     = this.lighter.bind(this);
+    this.darker      = this.darker.bind(this);
   }
 
   createColor(){
-    this.state.colors.push({rgba: '#098'});
+    let randomColor = Math.floor(Math.random()* 255 + 1);
+    this.state.colors.push({rgba: `hsl(${randomColor}, 70%, 70%)`});
     let colors = this.state.colors;
+    console.log(colors);
     this.setState(colors);
   }
 
@@ -29,6 +33,19 @@ class App extends Component {
     this.state.colors.splice(i, 1);
     let colors = this.state.colors;
     this.setState(colors);
+
+  }
+
+  lighter(i){
+    console.log(i);
+    console.log(this.state.colors[i]);
+    let color = Color(this.state.colors[i].rgba);
+    console.log(color);
+    let rgb = color.rgb();
+    console.log(rgb);
+  }
+
+  darker(i){
 
   }
 
@@ -49,9 +66,15 @@ class App extends Component {
           <ul className="colors">
             {this.state.colors.map((color, i)=>{
                 let bcg = {background: color.rgba};
+                  console.log(bcg);
                 return (
-                <li className="colors__one" key={"color" + i} style={bcg} onClick={this.deleteColor.bind(this, i)}>
-                    {color.rgba}
+                <li className="colors__one" key={"color" + i} style={bcg}>
+                  {color.rgba}
+                  <a onClick={this.deleteColor.bind(this, i)}>X</a>
+                  <br/>
+                  <a onClick={this.lighter.bind(this, i)}>lighter</a>
+                  <br/>
+                  <a onClick={this.darker.bind(this, i)}>darker</a>
                 </li>
                 )}
             )}
