7baf2368dd78186a0a99c96559742260d2d6793d
Stacy Chen <chiufin@gmail.com>
2016-12-17 18:33:39 +0800

    add lighter and darker fnt

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Color from 'color';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Color from 'color'
+import _ from 'lodash'
 
 class App extends Component {
   constructor(props) {
@@ -9,10 +10,10 @@ class App extends Component {
     this.state = {
       date: new Date(),
       colors: [
-        {rgba: 'hsl(255, 70%, 70%)'},
-        {rgba: 'hsl(155, 70%, 70%)'},
-        {rgba: 'hsl(215, 70%, 70%)'},
-        {rgba: 'hsl(50, 70%, 70%)'}
+        {rgb: '#123456'},
+        {rgb: '#234567'},
+        {rgb: '#345678'},
+        {rgb: '#456789'}
       ]
     };
     this.createColor = this.createColor.bind(this);
@@ -23,7 +24,7 @@ class App extends Component {
 
   createColor(){
     let randomColor = Math.floor(Math.random()* 255 + 1);
-    this.state.colors.push({rgba: `hsl(${randomColor}, 70%, 70%)`});
+    this.state.colors.push({rgb: `hsl(${randomColor}, 70%, 70%)`});
     let colors = this.state.colors;
     console.log(colors);
     this.setState(colors);
@@ -37,17 +38,58 @@ class App extends Component {
   }
 
   lighter(i){
-    console.log(i);
-    console.log(this.state.colors[i]);
-    let color = Color(this.state.colors[i].rgba);
-    console.log(color);
-    let rgb = color.rgb();
-    console.log(rgb);
+    let stateColor = this.state.colors;
+    let thisHex = stateColor[i].rgb;
+
+    let rgbObj = Color(thisHex);
+    let hslObj = rgbObj.hsl();
+
+    let lighterColor = _.parseInt(hslObj.color[2])+10;
+    hslObj.color[2] = lighterColor;
+
+    let rgbArray = hslObj.rgb().array();
+    let hex = "#";
+    for(let i = 0 ; i< rgbArray.length ; i++){
+      console.log(rgbArray[i]);
+      hex += this.noOneWord(rgbArray[i]);
+      console.log(hex);
+    }
+
+    stateColor[i].rgb = hex;
+    this.setState(stateColor);
   }
 
   darker(i){
+    let stateColor = this.state.colors;
+    let thisHex = stateColor[i].rgb;
 
+    let rgbObj = Color(thisHex);
+    let hslObj = rgbObj.hsl();
+
+    let darkerColor = _.parseInt(hslObj.color[2])-10;
+    hslObj.color[2] = darkerColor;
+
+    let rgbArray = hslObj.rgb().array();
+    let hex = "#";
+    for(let i = 0 ; i< rgbArray.length ; i++){
+      console.log(rgbArray[i]);
+      hex += this.noOneWord(rgbArray[i]);
+      console.log(hex);
+    }
+
+    stateColor[i].rgb = hex;
+    this.setState(stateColor);
   }
+
+  noOneWord(num){
+    let string = _.parseInt(num).toString(16);
+    if(string.length <= 1){
+      string = '0' + string;
+    }
+    return string
+  }
+
+
 
   render() {
 	return (
@@ -65,11 +107,11 @@ class App extends Component {
           <button onClick={this.createColor}>create new</button>
           <ul className="colors">
             {this.state.colors.map((color, i)=>{
-                let bcg = {background: color.rgba};
+                let bcg = {background: color.rgb};
                   console.log(bcg);
                 return (
                 <li className="colors__one" key={"color" + i} style={bcg}>
-                  {color.rgba}
+                  {color.rgb}
                   <a onClick={this.deleteColor.bind(this, i)}>X</a>
                   <br/>
                   <a onClick={this.lighter.bind(this, i)}>lighter</a>
