9b1202866b547af7b36640e051e14919adb1ade3
Stacy Chen <chiufin@gmail.com>
2016-12-18 21:19:37 +0800

    add preview new color

--- src/App.js
+++ src/App.js
@@ -10,16 +10,21 @@ class App extends Component {
     this.state = {
       date: new Date(),
       colors: [
-        {rgb: '#123456'},
-        {rgb: '#234567'},
-        {rgb: '#345678'},
-        {rgb: '#456789'}
+        {rgb: '#456456'},
+        {rgb: '#eded11'},
+        {rgb: '#113678'},
+        {rgb: '#2200ee'}
       ]
     };
     this.createColor = this.createColor.bind(this);
     this.deleteColor = this.deleteColor.bind(this);
     this.lighter     = this.lighter.bind(this);
     this.darker      = this.darker.bind(this);
+    this.moreColor   = this.moreColor.bind(this);
+    this.lessColor   = this.lessColor.bind(this);
+    this.resetAll    = this.resetAll.bind(this);
+    this.changePrevColor = this.changePrevColor.bind(this);
+    this.changeNextColor = this.changeNextColor.bind(this);
   }
 
   createColor(){
@@ -89,11 +94,109 @@ class App extends Component {
     return string
   }
 
+  moreColor(i){
+    let stateColor = this.state.colors;
+    let thisHex = stateColor[i].rgb;
+
+    let rgbObj = Color(thisHex);
+    let hslObj = rgbObj.hsl();
+
+    let darkerColor = _.parseInt(hslObj.color[1])+10;
+    hslObj.color[1] = darkerColor;
+
+    let rgbArray = hslObj.rgb().array();
+    let hex = "#";
+    for(let i = 0 ; i< rgbArray.length ; i++){
+      console.log(rgbArray[i]);
+      hex += this.noOneWord(rgbArray[i]);
+      console.log(hex);
+    }
+
+    stateColor[i].rgb = hex;
+    this.setState(stateColor);
+  }
+
+  lessColor(i){
+    let stateColor = this.state.colors;
+    let thisHex = stateColor[i].rgb;
+
+    let rgbObj = Color(thisHex);
+    let hslObj = rgbObj.hsl();
+
+    let lighterColor = _.parseInt(hslObj.color[1])-10;
+    hslObj.color[1] = lighterColor;
+
+    let rgbArray = hslObj.rgb().array();
+    let hex = "#";
+    for(let i = 0 ; i< rgbArray.length ; i++){
+      console.log(rgbArray[i]);
+      hex += this.noOneWord(rgbArray[i]);
+      console.log(hex);
+    }
+
+    stateColor[i].rgb = hex;
+    this.setState(stateColor);
+  }
+
+  changePrevColor(i){
+    let stateColor = this.state.colors;
+    let thisHex = stateColor[i].rgb;
+
+    let rgbObj = Color(thisHex);
+    let hslObj = rgbObj.hsl();
+
+    let lighterColor = _.parseInt(hslObj.color[0])-10;
+    hslObj.color[0] = lighterColor;
+
+    let rgbArray = hslObj.rgb().array();
+    let hex = "#";
+    for(let i = 0 ; i< rgbArray.length ; i++){
+      console.log(rgbArray[i]);
+      hex += this.noOneWord(rgbArray[i]);
+      console.log(hex);
+    }
+
+    stateColor[i].rgb = hex;
+    this.setState(stateColor);
+
+  }
+
+  changeNextColor(i){
+    let stateColor = this.state.colors;
+    let thisHex = stateColor[i].rgb;
+
+    let rgbObj = Color(thisHex);
+    let hslObj = rgbObj.hsl();
+
+    let lighterColor = _.parseInt(hslObj.color[0])+10;
+    hslObj.color[0] = lighterColor;
+
+    let rgbArray = hslObj.rgb().array();
+    let hex = "#";
+    for(let i = 0 ; i< rgbArray.length ; i++){
+      console.log(rgbArray[i]);
+      hex += this.noOneWord(rgbArray[i]);
+      console.log(hex);
+    }
+
+    stateColor[i].rgb = hex;
+    this.setState(stateColor);
+  }
+
+  resetAll(){
+
+  }
+
+  keypressLighter(i, e){
+    console.log(i);
+    console.log(e);
+  }
+
 
 
   render() {
 	return (
-	  <div className="App">
+	  <div className="App" onKeyPress={this.keypressLighter}>
 		<div className="App-header">
 		  <img src={logo} className="App-logo" alt="logo" />
 		  <h2>Color picker</h2>
@@ -104,23 +207,48 @@ class App extends Component {
 		<div>
           <h2> color list</h2>
           <p>{this.state.date.toLocaleTimeString()}</p>
-          <button onClick={this.createColor}>create new</button>
           <ul className="colors">
             {this.state.colors.map((color, i)=>{
                 let bcg = {background: color.rgb};
                   console.log(bcg);
                 return (
-                <li className="colors__one" key={"color" + i} style={bcg}>
-                  {color.rgb}
+                <li className="colors__list" key={"color" + i}>
+                  <div className="colors__list--one" style={bcg}>{color.rgb}
+                    <div className="colors__preview">
+                      <div className="colors__preview--item colors__preview--north"></div>
+                      <div className="colors__preview--item colors__preview--south"></div>
+                      <div className="colors__preview--item colors__preview--east"></div>
+                      <div className="colors__preview--item colors__preview--west"></div>
+                      <div className="colors__preview--item colors__preview--prevColor"></div>
+                      <div className="colors__preview--item colors__preview--nextColor"></div>
+                    </div>
+
+                  </div>
                   <a onClick={this.deleteColor.bind(this, i)}>X</a>
                   <br/>
                   <a onClick={this.lighter.bind(this, i)}>lighter</a>
                   <br/>
                   <a onClick={this.darker.bind(this, i)}>darker</a>
+                  <br/>
+                  <a onClick={this.lessColor.bind(this, i)}>less color</a>
+                  <br/>
+                  <a onClick={this.moreColor.bind(this, i)}>more color</a>
+                  <br/>
+                  <a onClick={this.changePrevColor.bind(this, i)}>prev color</a>
+                  <br/>
+                  <a onClick={this.changeNextColor.bind(this, i)}>next color</a>
+                  <br/>
+                  <a onClick={this.resetAll.bind(this, i)}>reset all</a>
+
+
                 </li>
                 )}
             )}
+                <li className="color__list">
+                  <div className="colors__list--one colors__list--dash" onClick={this.createColor}>new</div>
+                </li>
           </ul>
+          <button>Save</button>
 		</div>
 	  </div>
 	);
